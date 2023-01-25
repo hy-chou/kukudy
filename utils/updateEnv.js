86986c3d42dc89586d80c39bdac15ad464c3b012
HsuanYu CHOU <55382796+hy-chou@users.noreply.github.com>
2023-01-25 21:16:16 +0800

    add updateEnv.js, for getting a new access token

---
+++ utils/updateEnv.js
@@ -0,0 +1,18 @@
+const axios = require('axios');
+const { getTS, writeData } = require('./utils');
+
+require('dotenv').config({ path: '../.env' });
+
+if (require.main === module) {
+  axios.post('https://id.twitch.tv/oauth2/token', {
+    client_id: process.env.CLIENT_ID,
+    client_secret: process.env.CLIENT_SECRET,
+    grant_type: 'client_credentials',
+  })
+    .then((res) => res.data.access_token)
+    .then(async (token) => {
+      const ts = getTS();
+      await writeData('../.env', `ACCESS_TOKEN="${token}"  # ${ts}\n`);
+    })
+    .catch(() => { process.exitCode = 1; });
+}
