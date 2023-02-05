ff8f3784996ec2cc8128d3f0eec231ebaab315e4
HsuanYu CHOU <55382796+hy-chou@users.noreply.github.com>
2023-02-05 13:24:39 +0800

    mod book.sh, do not logIP.sh and style description

--- scripts/book.sh
+++ scripts/book.sh
@@ -3,17 +3,11 @@
 if [ $# -ne 3 ] ; then
     echo -e "
 SYNOPSIS
-    bash ./scripts/book.sh TARGET_DIR CHANNEL_COUNT ROUND_COUNT
+    bash book.sh TARGET_DIR CHANNEL_COUNT ROUND_COUNT
 
-NOTES
-    book.sh can be scheduled to run at a specific time by creating a file in the
-    /etc/cron.d directory with the following content:
-
-SHELL=/bin/sh
-PATH=$PATH
-DIR_K=$PWD
-
-49 15 04 02 * $USER bash \${DIR_K}/scripts/book.sh playground 100 1
+DESCRIPTION
+    book.sh collects at least CHANNEL_COUNT channels for ROUND_COUNT rounds and
+    stores the data inside the kukudy/TARGET_DIR/ directory.
 "
     exit 1
 fi
@@ -27,12 +21,8 @@ ROUND_COUNT=$3
 mkdir "${TARGET_DIR}" || exit 1
 cd "${TARGET_DIR}" || exit 1
 
-bash ../scripts/logIP.sh
-
 for _ in $(seq "${ROUND_COUNT}")
 do
     node ../updateStreams.js "${CHANNEL_COUNT}"
     node ../updateEdges.js
 done
-
-bash ../scripts/logIP.sh
