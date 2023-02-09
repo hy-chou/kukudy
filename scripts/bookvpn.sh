6782c3b24b76428bcc1ee3fc2a33a51cbdc41e1f
HsuanYu CHOU <55382796+hy-chou@users.noreply.github.com>
2023-02-09 14:41:21 +0800

    fix bookvpn.sh, corrected description

--- scripts/bookvpn.sh
+++ scripts/bookvpn.sh
@@ -7,8 +7,8 @@ SYNOPSIS
 
 DESCRIPTION
     bookvpn.sh connects with the server whose id is CONFIG_ID, collects at least
-    CHANNEL_COUNT channels for ROUND_COUNT rounds and stores the data inside the
-    kukudy/TARGET_DIR/ directory.
+    CHANNEL_COUNT channels and stores the data inside the kukudy/TARGET_DIR/
+    directory.
 "
     exit 1
 fi
