9022b6f6f8219419b7bdbbc91ffdcdedba178384
HsuanYu CHOU <55382796+hy-chou@users.noreply.github.com>
2023-02-13 17:08:35 +0800

    mod utils.js, add parseExtXTwitchInfo()

--- utils/utils.js
+++ utils/utils.js
@@ -14,9 +14,22 @@ const url2hostname = (url) => {
   return url.slice(p, q);
 };
 
+const parseExtXTwitchInfo = (usherM3U8) => {
+  const info = usherM3U8.split('\n')
+    .find((line) => line.startsWith('#EXT-X-TWITCH-INFO:'))
+    .slice(19)
+    .split(',')
+    .map((kv) => {
+      const p = kv.indexOf('=');
+      return [kv.slice(0, p), kv.slice(p + 2, -1)]
+    });
+
+  return Object.fromEntries(info);
+};
+
 const writeData = (path, data) => mkdir(dirname(path), { recursive: true })
   .then(() => appendFile(path, data));
 
 module.exports = {
-  getTS, sleep, url2hostname, writeData,
+  getTS, sleep, url2hostname, parseExtXTwitchInfo, writeData,
 };
