3494e9a3fa713baa34e96da47305703e5b17e9fc
HsuanYu CHOU <55382796+hy-chou@users.noreply.github.com>
2023-02-13 17:18:50 +0800

    mod updateInfo, reorganize

--- updateInfo.js
+++ updateInfo.js
@@ -1,7 +1,7 @@
 const { readdir, readFile } = require('node:fs/promises');
 
 const KAPI = require('./utils/API');
-const { getTS, writeData, sleep } = require('./utils/utils');
+const { getTS, writeData, sleep, parseExtXTwitchInfo } = require('./utils/utils');
 
 const loadUserLogins = async () => {
   const file = await readdir('./ulgs')
@@ -12,44 +12,40 @@ const loadUserLogins = async () => {
     .then((content) => content.slice(0, -1).split('\n'));
 };
 
-const updateEdges = async () => {
-  const ts0 = getTS().replaceAll(':', '.');
-  const dumpPath = `./dump/getVEH/${ts0}.tsv`;
+const getEdgeInfo = async (userLogin) => {
+  try {
+    const resPAT = await KAPI.reqPlaybackAccessToken(userLogin);
+    const sPAToken = resPAT.data.data.streamPlaybackAccessToken;
+    const resUsher = await KAPI.reqUsherM3U8(sPAToken, userLogin);
+    const info = parseExtXTwitchInfo(resUsher.data);
+
+    return info;
+  } catch (error) {
+    if (error.response) {
+      return {
+        headers: error.response.headers,
+        data: error.response.data,
+      };
+    }
+    return error.message;
+  };
+}
+
+const updateInfo = async () => {
+  const ts = getTS().replaceAll(':', '.');
+  const edgsPath = `./edgs/${ts}.tsv`;
 
   const userLogins = await loadUserLogins();
 
   userLogins.forEach(async (userLogin, index) => {
     await sleep(index * 40); // 25 Hz
 
-    try {
-      const ts1 = getTS();
-      const resPAT = await KAPI.reqPlaybackAccessToken(userLogin);
-      const ts2 = getTS();
+    const info = await getEdgeInfo(userLogin);
 
-      writeData(
-        dumpPath,
-        `${ts1}\t${ts2}\treqPAT\t${userLogin}\t`
-          + `${JSON.stringify(resPAT.headers)}\t`
-          + `${JSON.stringify(resPAT.data)}\n`,
-      );
-
-      const sPAToken = resPAT.data.data.streamPlaybackAccessToken;
-      const ts3 = getTS();
-      const resUsherM3U8 = await KAPI.reqUsherM3U8(sPAToken, userLogin);
-      const ts4 = getTS();
-
-      writeData(
-        dumpPath,
-        `${ts3}\t${ts4}\treqUsherM3U8\t${userLogin}\t`
-            + `${JSON.stringify(resUsherM3U8.headers)}\t`
-            + `${JSON.stringify(resUsherM3U8.data)}\n`,
-      );
-    } catch (error) {
-      // continue regardless of error
-    }
+    writeData(edgsPath, `${getTS()}\t${userLogin}\t${JSON.stringify(info)}\n`);
   });
 };
 
 if (require.main === module) {
-  updateEdges();
+  updateInfo();
 }
