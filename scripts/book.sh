81e0d6e7e8baf91a43d7ca8dd17abda827497968
HsuanYu CHOU <55382796+hy-chou@users.noreply.github.com>
2023-02-24 11:44:12 +0800

    mod book.sh, remove the ROUND_COUNT option

--- scripts/book.sh
+++ scripts/book.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-if ! [[ $# -eq 3 && $2 =~ ^[0-9]+$ && $3 =~ ^[0-9]+$ ]] ; then
-    echo "book.sh: invalid input \"$*\"
+if ! [[ $# -eq 2 && $2 =~ ^[0-9]+$ ]] ; then
+    echo "book.sh: invalid arguments \"$*\"
 
 SYNOPSIS
-    bash book.sh TARGET_DIR CHANNEL_COUNT ROUND_COUNT
+    bash book.sh TARGET_DIR CHANNEL_COUNT
 
 DESCRIPTION
-    book.sh collects at least CHANNEL_COUNT channels for ROUND_COUNT rounds and
-    stores the data inside the kukudy/TARGET_DIR/ directory." >&2
+    book.sh collects at least CHANNEL_COUNT channels and stores the data inside
+    the kukudy/TARGET_DIR/ directory." >&2
     exit 1
 fi
 
@@ -16,12 +16,9 @@ cd "$(dirname "$0")/.." || exit 1
 
 TARGET_DIR=$1
 CHANNEL_COUNT=$2
-ROUND_COUNT=$3
 
 mkdir -p "${TARGET_DIR}" || exit 1
 cd "${TARGET_DIR}" || exit 1
 
-for _ in $(seq "${ROUND_COUNT}") ; do
-    node ../updateStreams.js "${CHANNEL_COUNT}"
-    node ../updateInfo.js
-done
+node ../updateStreams.js "${CHANNEL_COUNT}"
+node ../updateInfo.js
