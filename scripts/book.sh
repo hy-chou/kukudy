2fab66851355d92d05ad5aaf94fbe40b89b6ef93
HsuanYu CHOU <55382796+hy-chou@users.noreply.github.com>
2023-03-09 11:14:54 +0800

    mod book.sh, functionize

--- scripts/book.sh
+++ scripts/book.sh
@@ -12,25 +12,39 @@ DESCRIPTION
     exit 1
 fi
 
-cd "$(dirname "$0")/.." || exit 1
+init() {
+    cd "$(dirname "$0")/.." || exit 1
 
-DIRECTORY=$1
-CHANNEL_COUNT=$2
+    DIRECTORY=$1
+    CHANNEL_COUNT=$2
 
-mkdir -p "${DIRECTORY}" || exit 1
-cd "${DIRECTORY}" || exit 1
+    mkdir -p "${DIRECTORY}" || exit 1
+    cd "${DIRECTORY}" || exit 1
+}
 
-{
-TS_NOW=$(date +"%s")
-TS_LAST_US=$(stat --format="%Y" ./ulgs 2> /dev/null || echo 0)
+updateStreams() {
+    TS_NOW=$(date +"%s")
+    TS_LAST_US=$(stat --format="%Y" ./ulgs 2> /dev/null || echo 0)
 
-if [[ $(( TS_NOW - TS_LAST_US )) -ge $(( 10 * 60 )) ]] ; then
-    echo -en "$(date -u +"%FT%T.%3NZ")\tuS starting\n"
-    node ../updateStreams.js "${CHANNEL_COUNT}"
-    echo -en "$(date -u +"%FT%T.%3NZ")\tuS ended\n"
-fi
+    if [[ $(( TS_NOW - TS_LAST_US )) -ge $(( 10 * 60 )) ]] ; then
+        echo -en "$(date -u +"%FT%T.%3NZ")\tuS starting\n"
+        node ../updateStreams.js "${CHANNEL_COUNT}"
+        echo -en "$(date -u +"%FT%T.%3NZ")\tuS ended\n"
+    fi
+}
 
-echo -en "$(date -u +"%FT%T.%3NZ")\tuI starting\n"
-node ../updateInfo.js
-echo -en "$(date -u +"%FT%T.%3NZ")\tuI ended\n"
-} >> log.out 2>> log.err
+updateInfo() {
+    echo -en "$(date -u +"%FT%T.%3NZ")\tuI starting\n"
+    node ../updateInfo.js
+    echo -en "$(date -u +"%FT%T.%3NZ")\tuI ended\n"
+}
+
+main() {
+    init "$@"
+    {
+        updateStreams
+        updateInfo
+    } >> log.out 2>> log.err
+}
+
+main "$@"
